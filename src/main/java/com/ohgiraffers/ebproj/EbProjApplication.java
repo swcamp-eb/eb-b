cd3d9e6103d68a14c7b1ab1a69aa35439480be00
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-05-13 12:43:54 +0900

    first push

---
+++ src/main/java/com/ohgiraffers/ebproj/EbProjApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.ebproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/ebproj/EbProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.ebproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
